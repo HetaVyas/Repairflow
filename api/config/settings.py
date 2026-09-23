c868c1cfec838792f568ebe52282a49cb249fdd0
HetaVyas <hetvyaas03@gmail.com>
2026-09-23 18:57:37 +0530

    fixed allowed_hosts

--- api/config/settings.py
+++ api/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-!o)43h8!vomsv*wy=1w-tpwe2%y14w&@bu!l=%2x@#*(84gqb2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
