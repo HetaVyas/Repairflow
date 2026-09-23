2c0244c8aa73343be0446cd8709df1624fdf2ede
HetaVyas <hetvyaas03@gmail.com>
2026-09-23 18:36:04 +0530

    debug fixed

--- api/config/settings.py
+++ api/config/settings.py
@@ -25,7 +25,7 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = 'django-insecure-!o)43h8!vomsv*wy=1w-tpwe2%y14w&@bu!l=%2x@#*(84gqb2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = []
 
