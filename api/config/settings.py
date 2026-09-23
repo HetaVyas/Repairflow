57048ccb4386ad5535db4e2b17554428021a652c
HetaVyas <hetvyaas03@gmail.com>
2026-09-23 20:12:58 +0530

    endpoint clash resolved

--- api/config/settings.py
+++ api/config/settings.py
@@ -20,6 +20,7 @@ load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
+APPEND_SLASH = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-!o)43h8!vomsv*wy=1w-tpwe2%y14w&@bu!l=%2x@#*(84gqb2'
